6c3a1f0d86d83caf76f51d281ff66e8b745ee469
arbi1994 <arbi.1994@hotmail.com>
2021-05-11 18:37:11 +1000

    added smooth scrolling. Applied effect to hide navbar of scrolldown and show when scroll up, with a bit of styling.

--- main.js
+++ main.js
@@ -9,3 +9,25 @@ burgerIcon.addEventListener("click", () => {
 closeIcon.addEventListener("click", () => {
     menu.classList.remove("open")
 })
+
+/* Show/Hide navbar when scrolling */
+const navBar = document.querySelector(".navbar")
+let prevScrollPos = window.pageYOffset
+
+window.addEventListener("scroll", () => {
+  let currScrollPos = window.pageYOffset
+  console.log(currScrollPos)
+
+  if(prevScrollPos > currScrollPos){
+    //navBar.style.top = "0"
+    navBar.style.opacity = "1"
+    navBar.style.backdropFilter = "blur(20px)";
+    if(currScrollPos == 0){
+      navBar.style.backdropFilter = "blur(0px)";
+    }
+  }else{
+    navBar.style.opacity = "0"
+  }
+
+  prevScrollPos = currScrollPos
+})
