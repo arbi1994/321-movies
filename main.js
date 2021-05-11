9c3a81f0bd8a3a60fb648ea4901b59145ac49a8f
arbi1994 <arbi.1994@hotmail.com>
2021-05-11 21:18:40 +1000

    added search bar as a popup when we click on the search icon. Had to set visibility property as well on the scrolling event to prevent the navbar from covering genres buttons.

--- main.js
+++ main.js
@@ -20,6 +20,7 @@ window.addEventListener("scroll", () => {
 
   if(prevScrollPos > currScrollPos){
     //navBar.style.top = "0"
+    navBar.style.visibility = "visible"
     navBar.style.opacity = "1"
     navBar.style.backdropFilter = "blur(20px)";
     if(currScrollPos == 0){
@@ -27,7 +28,23 @@ window.addEventListener("scroll", () => {
     }
   }else{
     navBar.style.opacity = "0"
+    navBar.style.visibility = "hidden"
   }
 
   prevScrollPos = currScrollPos
+})
+
+/* Show search popup */
+const searchIcon = document.querySelector(".hero__search")//search icon
+const popup = document.querySelector(".search__popup")
+
+searchIcon.addEventListener("click", () => {
+  //open up search popup
+  popup.classList.add("show")
+})
+
+const closePopup = document.querySelector(".fa-times")//close icon
+closePopup.addEventListener("click", () => {
+  //close down search popup
+  popup.classList.remove("show")
 })
