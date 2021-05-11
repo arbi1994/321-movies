3e5c90e5c09e163ca88a5d728e823cc9be4f284d
arbi1994 <arbi.1994@hotmail.com>
2021-05-11 22:41:11 +1000

    burger menu set relative to the body so that it's independent from the main navbar. Fixed the nav bar position in the grid for laptop andmobile view

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 /* BURGER MENU */
 const burgerIcon = document.querySelector(".navbar__burger img") //open icon
 const closeIcon = document.querySelector(".menu--mobile__close") //close icon
-const menu = document.querySelector(".navbar__menu--mobile") //menu
+const menu = document.querySelector(".nav__menu--mobile") //menu
+const navLinks = document.querySelectorAll(".menu__list a")
 
 burgerIcon.addEventListener("click", () => {
     menu.classList.add("open")
@@ -9,30 +10,37 @@ burgerIcon.addEventListener("click", () => {
 closeIcon.addEventListener("click", () => {
     menu.classList.remove("open")
 })
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    menu.classList.remove("open")
+  })
+})
 
 /* Show/Hide navbar when scrolling */
 const navBar = document.querySelector(".navbar")
 let prevScrollPos = window.pageYOffset
 
 window.addEventListener("scroll", () => {
+  showHideNavbar()
+})
+
+function showHideNavbar(){
   let currScrollPos = window.pageYOffset
   console.log(currScrollPos)
 
   if(prevScrollPos > currScrollPos){
-    //navBar.style.top = "0"
     navBar.style.visibility = "visible"
     navBar.style.opacity = "1"
     navBar.style.backdropFilter = "blur(20px)";
-    if(currScrollPos == 0){
-      navBar.style.backdropFilter = "blur(0px)";
-    }
+
+    if(currScrollPos == 0) navBar.style.backdropFilter = "blur(0px)"
   }else{
     navBar.style.opacity = "0"
     navBar.style.visibility = "hidden"
   }
 
   prevScrollPos = currScrollPos
-})
+}
 
 /* Show search popup */
 const searchIcon = document.querySelector(".hero__search")//search icon
