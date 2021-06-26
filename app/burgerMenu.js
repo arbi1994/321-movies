33cc8344c21ba4174dd7ed4d87f6096225fb2fc6
arbi1994 <arbi.1994@hotmail.com>
2021-06-26 20:41:40 +1000

    Enabled scroll to the maindocument, after clicking on a nav link from the burger menu

--- app/burgerMenu.js
+++ app/burgerMenu.js
@@ -1,25 +1,27 @@
 /* BURGER MENU */
 (function(){
-    const burgerIcon = document.querySelector(".navbar__burger img") //open icon
-    const closeIcon = document.querySelector(".menu--mobile__close") //close icon
-    const menu = document.querySelector(".nav__menu--mobile") //menu
-    const navLinks = document.querySelectorAll(".menu__list a")
+  const burgerIcon = document.querySelector(".navbar__burger img") //open icon
+  const closeIcon = document.querySelector(".menu--mobile__close") //close icon
+  const menu = document.querySelector(".nav__menu--mobile") //menu
+  const navLinks = document.querySelectorAll(".menu__list a")
   
-    // const targetElement = document.querySelector(".nav__menu--mobile")
+  // const targetElement = document.querySelector(".nav__menu--mobile")
   
-    burgerIcon.addEventListener("click", () => {
-        menu.classList.add("open")
-        disableScroll(menu)
+  burgerIcon.addEventListener("click", () => {
+    menu.classList.add("open")
+    disableScroll(menu)
+  })
+
+  closeIcon.addEventListener("click", () => {
+    menu.classList.remove("open")
+    enableScroll(menu)
+  })
+
+  navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+      menu.classList.remove("open")
+      enableScroll(menu)
     })
-    closeIcon.addEventListener("click", () => {
-        menu.classList.remove("open")
-        enableScroll(menu)
-    })
-    navLinks.forEach(link => {
-      link.addEventListener("click", () => {
-        menu.classList.remove("open")
-      })
-    })
-  
-    
-  })()
+  })
+
+})()
